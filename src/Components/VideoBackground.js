4d4ae58e8893cba4532b8ee06063779409431a90
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-10-19 13:04:09 +0530

    Info button click handler

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -9,7 +9,7 @@ const VideoBackground = ({movieId}) => {
   return (
     <div className=''>
         
-        <iframe className='w-full aspect-video ' src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=1&mute=1&cc_load_policy=0"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe className='w-full aspect-video ' src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=0&mute=1&cc_load_policy=0"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   )
 }
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VideoTitle = ({ title, overview }) => {
+  const [info, setInfo ] = useState(false);
+
+  const handleInfoButtonClick = ()=>{
+    setInfo(!info);
+  }
+
   return (
     <div className="pt-[5%] md:pt-[14%] px-4 md:px-12 absolute w-full aspect-video text-white bg-gradient-to-r from-black">
-      <h1 className="text-xl w-1/2 md:text-3xl font-semibold">{title}</h1>
-      <p className="hidden md:inline-block w-1/3 py-6 text-lg">{overview}</p>
-      <div className="flex flex-col md:flex-row my-4 md:my-0">
+      <h1 className="text-xl w-full md:w-1/2 md:text-3xl font-semibold">{title}</h1>
+      {info && <div>
+      <p className="w-full h-24 md:h-44 overflow-scroll md:w-1/2 py-2 md:py-6 text-md md:text-lg">{overview}</p>
+      </div>}
+      
+      <div className="flex flex-col md:flex-row my-1 md:my-6">
         <button className="bg-white bg-opacity-50 md:bg-opacity-100 text-white md:text-black hover:opacity-70 w-1/4 md:w-1/6 mt-2 md:mt-0 hover:scale-105 px-0 md:px-8 py-1 md:py-3 text-md md:text-xl rounded-lg">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,8 +32,8 @@ const VideoTitle = ({ title, overview }) => {
           </svg>
           Play
         </button>
-        <button className="bg-gray-500 bg-opacity-50 text-white w-1/4 md:w-1/6 mt-2 md:mt-0 hover:scale-105 px-1 md:px-8 py-1 md:py-3 text-md md:text-xl rounded-lg mx-0 md:mx-2">
-          <svg
+        <button onClick={handleInfoButtonClick} className="bg-gray-500 bg-opacity-50 text-white w-1/4 md:w-1/6 mt-2 md:mt-0 hover:scale-105 px-1 md:px-8 py-1 md:py-3 text-md md:text-xl rounded-lg mx-0 md:mx-2">
+          {!info? <> <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -38,7 +47,7 @@ const VideoTitle = ({ title, overview }) => {
               d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
             />
           </svg>
-          <span className="hidden md:inline-block">More </span><span> Info</span>
+          <span className="hidden md:inline-block">More </span><span> Info</span> </> :<span>Show less</span> } 
         </button>
       </div>
     </div>
